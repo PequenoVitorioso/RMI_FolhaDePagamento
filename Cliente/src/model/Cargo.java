828aabf0e68f205c312da0e27be6b55c3fc3b397
Paulo Vitor <paulovfbarros28@gmail.com>
2025-10-24 17:24:05 -0300

    adicionei o cargo model

---
+++ Cliente/src/model/Cargo.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Cargo {
+    private int id_cargo;
+    private String nome;
+    private String nivel;
+    private float salario;
+
+    public Cargo() {
+    }
+
+    public int getId_cargo() {
+        return id_cargo;
+    }
+
+    public void setId_cargo(int id_cargo) {
+        this.id_cargo = id_cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+}
